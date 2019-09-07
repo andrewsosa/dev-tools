06db0d2f2f082d82e010c570649a26196cbd8160
Andrew Sosa <andrew@andrewsosa.com>
2019-09-07 15:12:13 -0400

    Update package deps and new config file formats

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,7 @@
 module.exports = {
-  extends: ["airbnb-base", "plugin:prettier/recommended"],
-  plugins: ["prettier"],
-  rules: {
-    "no-console": "off",
-    "prettier/prettier": "error",
+  "extends": ["airbnb-base", "prettier"],
+  "plugins": ["prettier"],
+  "rules": {
+    "prettier/prettier": ["error"]
   },
-};
+}
