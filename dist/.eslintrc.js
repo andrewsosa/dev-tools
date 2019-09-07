b8149935e23eb59d6ddf949c6067fe0dfc3def56
Andrew Sosa <andrew@andrewsosa.com>
2019-09-07 15:11:59 -0400

    Remove old files

--- dist/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ["airbnb-base", "plugin:prettier/recommended"],
-  plugins: ["prettier"],
-  rules: {
-    "no-console": "off",
-    "prettier/prettier": "error",
-  },
-};
